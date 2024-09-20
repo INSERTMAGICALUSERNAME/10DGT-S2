150226cecb4e2b28b7644b130b218df1f0b4946a
INSERTMAGICALUSERNAME <Johann.shotbolt@kingsway.school.nz>
2024-09-20 14:50:24 +1200

    Add files via upload
    My first code

---
+++ Python code.py
@@ -0,0 +1,18 @@
+# showing how variables work
+# Author: Johann shotbolt
+# Date: 20 september 20204
+# version 1
+
+
+# Different types of variables 
+num_1 = 80 
+print # this displays what num_1 contains
+
+# store a string. 
+Hello= 'Hello '
+name = input('Type your name here')
+concatenation = Hello + name 
+print(concatenation)
+
+
+
