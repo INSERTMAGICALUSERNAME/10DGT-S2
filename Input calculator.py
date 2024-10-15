ef057bd09095ab16cdbdce76cfec06ecd4adca77
INSERTMAGICALUSERNAME <Johann.shotbolt@kingsway.school.nz>
2024-10-16 09:42:39 +1300

    Add files via upload

---
+++ Input calculator.py
@@ -0,0 +1,11 @@
+#Not really sure what this is
+# just messing around
+# 16 oct
+#author: johann Shotbolt
+
+number_1 = (int(input("Insert a number")))
+number_2 = (int(input("Insert another number")))
+sum = number_1 + number_2
+print (f'{number_1} + {number_2} = {sum}')
+
+
