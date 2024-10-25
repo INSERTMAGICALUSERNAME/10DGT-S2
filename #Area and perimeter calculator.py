b329f19a9af8791fcfe5dde5430e472dec7f7992
INSERTMAGICALUSERNAME <Johann.shotbolt@kingsway.school.nz>
2024-10-25 14:35:54 +1300

    Add files via upload
    V2

--- #Area and perimeter calculator.py
+++ #Area and perimeter calculator.py
@@ -1,11 +1,14 @@
 #Area and perimeter calculator 
 #Author: Johann Shobolt
 #Date: 2024/10/25
-#Version: 1
+#Version: 2
 
+
+print ()
+print ()
 print('This program will calculate the area and perimeter of your shape')
 def valid_num (question):
-    error = "whoops, that is not a valid number"
+    error = "that is not a valid number ಠ~ಠ\n"
     while True:
         try:
             response = float(input(question))
@@ -17,26 +20,33 @@ def valid_num (question):
         except ValueError:
             print (error)
     return response
-#Make this loop, dont be lazy 
-width = valid_num('enter your width\n')
-print (f'the width of your shape is {width}')
-height = valid_num('now enter the shapes height\n')
-perimeter = (width * 2) + (height * 2) 
-area = width * height 
-print(f'The perimeter of your shape is {perimeter}\n')
-print (f'The area of your shape is {area}')
-
-
-
-
-
-
-
-
-
-'''loopy_mc_looper = ''
+loopy_mc_looper = ''
 while loopy_mc_looper == '':
-    width = float(input('Enter the width of your shape'))
-    print (f'The Width of your shape is {width}')
-    height = float(input('Enter the height of your shape'))
-    print (f'The Height of your shape is {height}')'''
+    width = valid_num('Enter your width:\n')
+    print (f'The width of your shape is {width}')
+    height = valid_num('Now enter the shapes height:\n')
+    print (f'The Height of your shape is {height}\n')
+    perimeter = (width * 2) + (height * 2) 
+    area = width * height 
+    print(f'The perimeter of your shape is {perimeter}\n')
+    print (f'The area of your shape is {area}\n')
+
+    Go_again = input("Do you want me to ask again? (Y/N)").lower()
+    if Go_again == "no" or Go_again == "n":
+        loopy_mc_looper = Go_again
+    elif Go_again == "yes" or Go_again == "y":
+        print ("ok then\n")
+    
+    else:
+        print("Use a normal answer please ಠ~ಠ\n")
+
+
+
+
+
+
+
+
+
+
+
